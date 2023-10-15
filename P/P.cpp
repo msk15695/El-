5f3ac5169ec36141d66dff1e2af30bdcf2127304
msk15695 <147961332+msk15695@users.noreply.github.com>
2023-10-15 11:43:38 +0800

    Add files via upload

---
+++ P/P.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf_s("%d%d", & num1, & num2);
+	sum = num1 + num2;
+	printf("sum=%d\n", sum);
+	printf("El Psy Congroo");
+	return 0;
+}
