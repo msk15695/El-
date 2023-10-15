f514a3791e5d35dc1bc5bc8dbcaf8e6a18203b7f
msk15695 <147961332+msk15695@users.noreply.github.com>
2023-10-15 11:44:58 +0800

    Add files via upload

---
+++ s/s.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char arr1[] = "abc";//สýื้
+		char arr2[] = { 'a', 'b', 'c',0 };
+		printf("%d\n",strlen(arr1) );
+		printf("%d\n", strlen(arr2));
+
+		return 0;
+}
