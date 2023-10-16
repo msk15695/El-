6ca66424223c41ab3a6db9cdee8ce126b1d538d4
msk15695 <147961332+msk15695@users.noreply.github.com>
2023-10-16 19:30:59 +0800

    Add files via upload

---
+++ sizeof/size.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("%d", sizeof(char));
+	printf("%d", sizeof(short));
+	printf("%d", sizeof(int));
+	printf("%d", sizeof(long));
+	printf("%d", sizeof(long long));
+	printf("%d", sizeof(float));
+	printf("%d", sizeof(double));
+	return 1;
+}
