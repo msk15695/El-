c6b4653d229c5a4ee55a4b0807b4fdbb48da431d
msk15695 <147961332+msk15695@users.noreply.github.com>
2023-10-17 15:22:51 +0800

    Add files via upload

---
+++ 测试1/源1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+	{
+		if (b == 2)
+	    printf("hehe\n");
+	}
+	else
+	    printf("haha\n");
+	return 0;
+}
+//用代码块后，输出haha。
